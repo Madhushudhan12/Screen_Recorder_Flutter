f11a1deb11da623e8f5fd899d4c6526f06a22a64
Madhushudhan12 <137251008+Madhushudhan12@users.noreply.github.com>
2023-08-01 12:30:58 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\yashg\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\AppsAiT\\Flutter Project\\screen_recorder_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\yashg\\Downloads\\flutter"
+  "PROJECT_DIR=E:\\AppsAiT\\Flutter Project\\screen_recorder_app"
+  "FLUTTER_ROOT=C:\\Users\\yashg\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\AppsAiT\\Flutter Project\\screen_recorder_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\AppsAiT\\Flutter Project\\screen_recorder_app"
+  "FLUTTER_TARGET=E:\\AppsAiT\\Flutter Project\\screen_recorder_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\AppsAiT\\Flutter Project\\screen_recorder_app\\.dart_tool\\package_config.json"
+)
